e11020279133ea51eb5b75ab855be14f2d69a6dd
M-J-Renee <mrenee@umd.edu>
2026-06-16 14:08:24 -0400

    32 chan map update

--- A1x32.m
+++ A1x32.m
@@ -3,7 +3,7 @@
 % A1x32 neuronexus probe map for KS 
 shank_chs = [ 13 20 9 24 4 29 8 25 7 26 6 27 5 28 3 30 2 31 1 32 10 23 11 22 12 21 14 19 15 18 16 17];
  
-kcoords_map = num2cell(shank_chs);
+kcoords_map = ones(size(shank_chs));
 xcoords_map = num2cell(zeros(size(shank_chs)));
 
 ycoords_chs = 1500:-50:0; 
